4345aae471bd0ed2f90aaf9e35487105dae63a69
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-04-09 21:43:40 +0200

    Update useFecthCharacters.jsx

--- src/hooks/useFecthCharacters.jsx
+++ src/hooks/useFecthCharacters.jsx
@@ -29,3 +29,37 @@ const useFecthCharacters=(url)=>{
       }
       
 export default useFecthCharacters;
+
+/*import { useEffect, useState } from "react";
+
+const useFetchCharacter = (url) => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    // async function fetchURL () {
+    //   try {
+    //     const response = await fetch(url)
+    //     const dataJson = await response.json()
+    //     setData(dataJson)
+    //     setLoading(false)
+    //   } catch (err) {
+    //     console.log(setError(err))
+    //   }
+    // }
+    fetch(url)
+      .then(response => response.json())
+      .then(dataJson => {
+        setData(dataJson)
+        setLoading(false)
+      }).catch(err => {
+        console.log(setError(err))
+      }) 
+  }, [])
+  return {data, loading, error}
+  
+};
+
+export default useFetchCharacter
+*/
