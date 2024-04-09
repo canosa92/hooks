270137e6ab825a1bd670e84fea2656f5bb658c75
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-04-09 21:44:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,3 +35,41 @@ const RickAndMortyCharacter = useFecthCharacters(urlRick)
 }
 
 export default App;
+/*import useFetchCharacter from './hooks/useFecthCharacters.js';
+import './App.css';
+import CardApp from './components/CardApp.jsx';
+
+function App() {
+  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
+  const urlRick = 'https://rickandmortyapi.com/api/character/1';
+  const { data: pokemon, loading: pokemonLoading} = useFetchCharacter(urlPokemon)
+  const { data: rick, loading: rickLoading } = useFetchCharacter(urlRick)
+  const isLoading = <p>Loading</p>
+  
+
+
+  return (
+    <>
+    
+    {pokemonLoading 
+      ? (isLoading) 
+      : (<CardApp 
+          title='Personaje Pokemon'
+          name={pokemon.name}
+          image={pokemon.sprites.other.dream_world.front_default}
+          />)
+    }
+    {rickLoading 
+      ? (isLoading) 
+      : (<CardApp 
+          title='Personaje Rick and Morty'
+          name={rick.name}
+          image={rick.image}
+        />)
+    }    
+    </>
+    );
+}
+
+export default App;
+*/
